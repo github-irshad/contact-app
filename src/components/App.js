9fbe43e24d47ec9c1f4c03a3cf82aed25aa76f78
github-irshad <irshadaj08@gmail.com>
2024-12-21 14:11:53 +0530

    useEffect introduced

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
-import './App.css';
-import { ContactList } from './ContactList';
-import  AddContact  from './AddContact';
-import Header  from "./Header";
-import { useState } from 'react';
+import "./App.css";
+import { ContactList } from "./ContactList";
+import AddContact from "./AddContact";
+import Header from "./Header";
+import { useEffect, useState } from "react";
 
 function App() {
-
   const [contacts, setContacts] = useState([]);
-  const addContactHandler = (contact)=>{
-    setContacts([...contacts,contact]);
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
     // console.log(contact);
-    
-  }
+  };
+
+  const LOCAL_STORAGE_KEY = "contact";
+  
+  useEffect(() => {
+    const retrivedContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrivedContacts) setContacts(retrivedContacts);
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+
   return (
-    <div className='ui container'>
-      <Header/>
-      <AddContact addContactHandler = {addContactHandler}/>
-      <ContactList contacts = {contacts}/>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
